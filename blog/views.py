01f1a1b657a8eba6763261140e0a3bf4f76a409f
c137santos <clarasantosmf@gmail.com>
2023-12-15 15:06:34 -0300

    Agora templates!

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def portao(request):
+    return HttpResponse("Você chegou ao portão da casa")
+
+def sala(request):
+    return HttpResponse("Você chegou na sala. Senta no sofá!")
+
+def quarto(request):
+    return HttpResponse("Agora está no quarto, pode se deitar")
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
